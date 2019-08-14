349040e277504f93d9a23ad48696d7e5bd9dc983
Vladimir Loshchin <vladimirloshchin@cloud.upwork.com>
2019-08-14 10:46:41 +0700

    Implement HTTP session with persistent cookie to avoid blocking

--- elib-dl.py
+++ elib-dl.py
@@ -9,44 +9,57 @@ import json
 
 from bs4 import BeautifulSoup
 
-def loadBookInfo(url):
-    resp = requests.get(url)
-    soup = BeautifulSoup(resp.text, 'html.parser')
-    info = json.loads(soup.find_all('script')[1].text[15:-2])
-    pageids = [x['id'] for x in info["pages"]]
-    return pageids
+class ElibLoader:
 
-def loadpage(id, filename):
-    """
-    Load page on maximum zoom = 8
-    """
-    resp = requests.get('http://elib.shpl.ru/pages/%d/zooms/8' % id)
-    
-    with open(filename, 'wb') as f:
-        for chunk in resp.iter_content(1024):
-            f.write(chunk)
-    
+    def __init__(self, url):
+        self.bookUrl = url
+        self.session = requests.Session()
+        self.session.headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
+        }
 
-def loadbook(url):
-    pageids = loadBookInfo(url)
-    totalpages = len(pageids)
-    print("Info loaded: %d pages was found" % len(pageids))
-    
-    # Filename mask
-    fnmask = "{:0>%dd}.jpeg" % len(str(totalpages))
-    
-    for idx, id in enumerate(pageids):
-        pagenum = idx + 1
-        filename = fnmask.format(pagenum)
-        loadpage(id, filename)
-        print("Page %d successfully saved to %s" % (pagenum, filename))
+    def loadBookInfo(self):
+        resp = self.session.get(self.bookUrl)
+        soup = BeautifulSoup(resp.text, 'html.parser')
+        info = json.loads(soup.find_all('script')[1].text[15:-2])
+        pageids = [x['id'] for x in info["pages"]]
+        return pageids
+
+
+    def loadpage(self, id, filename):
+        """
+        Load page on maximum zoom = 8
+        """
+        resp = self.session.get('http://elib.shpl.ru/pages/%d/zooms/8' % id)
+
+        resp.raise_for_status()
+
+        with open(filename, 'wb') as f:
+            for chunk in resp.iter_content(1024):
+                f.write(chunk)
+
+
+    def loadbook(self):
+        pageids = self.loadBookInfo()
+        totalpages = len(pageids)
+        
+        print("Info loaded: %d pages was found" % len(pageids))
+
+        # Filename mask
+        fnmask = "{:0>%dd}.jpeg" % len(str(totalpages))
+
+        for idx, id in enumerate(pageids):
+            pagenum = idx + 1
+            filename = fnmask.format(pagenum)
+            self.loadpage(id, filename)
+            print("Page %d successfully saved to %s" % (pagenum, filename))
     
 def main():
     argparser = argparse.ArgumentParser(description='This program need to download books from elib.shpl.ru')
     argparser.add_argument('BOOK_URL', type=str)
     args = argparser.parse_args()
 
-    loadbook(args.BOOK_URL)
+    ElibLoader(args.BOOK_URL).loadbook()
 
 if __name__ == "__main__":
     main()
